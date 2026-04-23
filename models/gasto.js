86f7bc1b777a4bbc898926b6af62d37f1af1fe66
Ignacio Orellano <ignacio.orellano@cognitlabs.com>
2026-04-22 21:35:56 -0300

    feat: agregado de modelo para implementar funciones CRUD y validaciones en gastos

---
+++ models/gasto.js
@@ -0,0 +1,74 @@
+import fs from 'fs/promises';
+import path from 'path';
+import * as z from "zod";
+
+// ESQUEMA DE VALIDACION DE GASTO
+const Gasto = z.object({ 
+  // id: z.number("Id debe ser un numero!"),
+  // userId: z.number("userId debe ser un numero!"),
+  monto: z.number("El monto debe ser un numero!")
+    .gt(0, "EL monto debe ser mayor a cero!"),
+  titulo: z.string("Titulo debe ser un texto!")
+    .max(50, "El titulo debe tener como máximo 50 caracteres")
+    .min(5, "El titulo debe tener como mínimo 5 caracteres"),
+  descripcion: z.string("Descripcion debe ser un texto!").optional(),
+  // categoriaId: z.number()
+});
+
+// METODOS PRIVADOS - de acceso a datos, etc. No exportados, solo usados dentro de este módulo
+async function getGastos() {
+  const gastosJSON = await fs.readFile(path.join(process.cwd(), 'db/gastos.json'), 'utf-8');
+  const gastos = JSON.parse(gastosJSON);
+  return gastos
+}
+
+async function saveGastos(gastos) {
+  await fs.writeFile(path.join(process.cwd(), 'db/gastos.json'), JSON.stringify(gastos, null, 2), 'utf-8');
+}
+
+// FUNCIONES DE NEGOCIO - exportadas para ser usadas en rutas, controladores, etc
+export async function saveOneGasto(gasto){
+  const gastos = await getGastos();
+  gastos.push(gasto)
+  await saveGastos(gastos);
+}
+
+export async function getNextGastoId() {
+  const gastos = await getGastos();
+  const maxId = gastos.length;
+  return maxId + 1;
+}
+
+export async function getAllGastos() {
+  const gastos = await getGastos();
+  return gastos
+}
+
+export async function getOneGastoById(id) {
+  const gastos = await getGastos();
+  const gastoEncontrado = gastos.find(g => g.id === id)
+  return gastoEncontrado
+}
+
+export async function getAllGastosByUserId(userId){
+  const gastos = await getGastos();
+  const gastosEncontrados = gastos.filter(g => g.userId === userId)
+  return gastosEncontrados
+}
+
+
+// FUNCIONES DE VALIDACION - exportadas para ser usadas en rutas, controladores, etc
+export function validarGasto(gasto){
+  const resultado = Gasto.safeParse(gasto);
+  if(resultado.success === false){
+    return {
+      success: false,
+      errors: z.flattenError(resultado.error).fieldErrors
+    }
+  }
+  
+  return {
+    success: true
+  }
+}
+
